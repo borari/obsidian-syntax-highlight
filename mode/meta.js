4ec7aea82b7dda929835562984f2500a0739de17
borari <borari@fubared.net>
2023-05-10 17:03:25 -0400

    added {x} alias

--- mode/meta.js
+++ mode/meta.js
@@ -2,7 +2,7 @@
     {name: "APL", mime: "text/apl", mode: "apl", ext: ["dyalog", "apl"], alias: ["{apl}"]},
     {name: "PGP", mimes: ["application/pgp", "application/pgp-encrypted", "application/pgp-keys", "application/pgp-signature"], mode: "asciiarmor", ext: ["asc", "pgp", "sig"]}, alias: ["{pgp}"],
     {name: "ASN.1", mime: "text/x-ttcn-asn", mode: "asn.1", ext: ["asn", "asn1"], alias: ["{asn.1}"]},
-    {name: "Asterisk", mime: "text/x-asterisk", mode: "asterisk", file: /^extensions\.conf$/i},
+    {name: "Asterisk", mime: "text/x-asterisk", mode: "asterisk", file: /^extensions\.conf$/i}, alias: ["{asterisk}"],
     {name: "Brainfuck", mime: "text/x-brainfuck", mode: "brainfuck", ext: ["b", "bf"]},
     {name: "C", mime: "text/x-csrc", mode: "clike", ext: ["c", "h", "ino"]},
     {name: "C++", mime: "text/x-c++src", mode: "clike", ext: ["cpp", "c++", "cc", "cxx", "hpp", "h++", "hh", "hxx"], alias: ["cpp"]},
